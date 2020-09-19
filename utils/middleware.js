75a68af020a2dee012b96f379b8cd633a5b9b2a5
obasemola <obasemolaayodele@gmail.com>
2020-09-19 23:08:42 +0300

    module separation complete

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,9 +1,9 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method', method)
-  logger.info('Path', path)
-  logger.info('Body', Body)
+  logger.info('Method', request.method)
+  logger.info('Path', request.path)
+  logger.info('Body', request.ody)
   logger.info('____')
   next()
 }
