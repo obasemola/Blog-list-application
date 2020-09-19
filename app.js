9368af7073a3d8f809e66a93f346ece7cbef6632
obasemola <obasemolaayodele@gmail.com>
2020-09-19 18:04:57 +0300

    connction works from app.js!

---
+++ app.js
@@ -0,0 +1,33 @@
+const config = require('./utils/config');
+const express = require('express')
+const app = express()
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose')
+
+
+
+
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(() => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+
+
+
+
+
+
+
+app.use('/api/blogs', blogsRouter)
+
+
+
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
