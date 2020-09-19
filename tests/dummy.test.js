2c997c2025a5c739409174bdb8cf14cf44eb33f5
obasemola <obasemolaayodele@gmail.com>
2020-09-20 01:29:07 +0300

    more testing

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -15,6 +15,16 @@ const blogs = [
   { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
 ]
 
+const listWithFourBlogs = [
+  { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
+
+  { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
+  
+  { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
+  
+  { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -35,11 +45,18 @@ describe('total likes of', () => {
     }
   ]
 
-  test('of total like of one blog', () => {
+  test('of total likes of one blog', () => {
     expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+  });
+
+  test('of total likes of 4 blogs', () => {
+    expect(listHelper.totalLikes(listWithFourBlogs)).toBe(34)
   })
 
   test('of all 7 blogs in total', () => {
     expect(list_helper.totalLikes(blogs)).toBe(36)
   })
-})
+});
+
+
+describe('maximun likes of', )
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,10 +8,30 @@ const totalLikes = (blogs => {
   }, 0)
 })
 
+const favouriteBlog = (blogs) => {
+  const arr = blogs.map(blog => blog.likes);
+  const max = Math.max(...arr)
+  let favBlog;
 
+  blogs.map((blog) => {
+    if(blog.likes === max) {
+      favBlog = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+
+    }
+    else {
+      return
+    }
+  })
+  return favBlog
+}
 
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
