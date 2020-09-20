96008a883d6c2459ebfe70ca4445f62138b99d24
obasemola <obasemolaayodele@gmail.com>
2020-09-20 14:01:12 +0300

    reducing with lodash

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -6,7 +6,11 @@ const blogs = [
 
   { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
   
-  { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
+  { _id: "5a422b3a1b54a676234d17f9",
+  title: "Canonical string reduction",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12,
+  __v: 0 },
   
   { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 },
   
@@ -16,13 +20,34 @@ const blogs = [
 ]
 
 const listWithFourBlogs = [
-  { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
+  { _id: "5a422a851b54a676234d17f7",
+  title: "React patterns",
+  author: "Michael Chan",
+  url: "https://reactpatterns.com/",
+  likes: 7,
+  __v: 0 },
 
-  { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
+  { _id: "5a422aa71b54a676234d17f8",
+  title: "Go To Statement Considered Harmful",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+  likes: 5,
+  __v: 0 },
   
   { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
   
   { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 }
+]
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
 ]
 
 test('dummy returns one', () => {
@@ -34,16 +59,6 @@ test('dummy returns one', () => {
 
 
 describe('total likes of', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
 
   test('of total likes of one blog', () => {
     expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
@@ -59,4 +74,32 @@ describe('total likes of', () => {
 });
 
 
-describe('maximun likes of', )
+describe('maximun likes of', () => {
+  test('of maximum likes in 1 blog', () => {
+    const result = list_helper.favouriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  });
+
+  test('of maximum likes in 4 blogs', () => {
+    const result = list_helper.favouriteBlog(listWithFourBlogs);
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    })
+  });
+
+  test('of maximum likes in all blogs', () => {
+    const result = list_helper.favouriteBlog(blogs);
+
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    })
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1
 }
@@ -29,9 +31,29 @@ const favouriteBlog = (blogs) => {
   return favBlog
 }
 
+const mostBlogs = () => {
+  let blogsAuthors = blogs.map(blog => blog.author);
+  let reducedBlogsAuthors= _.reduce(blogsAuthors, (total, next) => {
+  total[next] = (total[next] || 0) + 1;
+
+  return total
+}, {});
+  const highestBlogsAuthor = Object.keys(reducedBlogsAuthors).reduce((a, b) => {
+    return reducedBlogsAuthors[a] > reducedBlogsAuthors[b] ? a : b
+  })
+
+  highestBlogNumberObject = {
+    author: highestBlogsAuthor,
+    blogs: reducedBlogsAuthors[highestBlogsAuthor]
+  }
+
+  return highestBlogNumberObject
+}
+
 
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs
 }
