1305e6077b6296ce887abd7578eac346b669f530
obasemola <obasemolaayodele@gmail.com>
2020-09-26 16:58:28 +0300

    created user routes and schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+mongoose.set('useFindAndModify', false)
+
+const userSchema = new mongoose.Schema({ 
+  name: String,
+  username: String,
+  passwordHash: String
+ });
+
+ userSchema.set('toJSON', {
+   transform: (document, returnedObject) => {
+     returnedObject.id = returnedObject._id.toString()
+     delete returnedObject._id
+     delete returnedObject.__v
+     delete returnedObject.passwordHash
+   }
+ });
+
+ module.exports = mongoose.model('User', userSchema);
